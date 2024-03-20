c45dbcb7c28b739890699e24d2aeb18055b26c0f
YismaelDiazH <105748001+YismaelDiazH@users.noreply.github.com>
2024-03-20 09:13:05 +0100

    cambio base url

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -2,10 +2,7 @@ import axios from "axios";
 import { createContext, useState } from "react";
 import { useEffect } from "react";
 
-const baseURL = process.env.NODE_ENV === 'development' 
-  ? 'http://localhost:8000/api' // URL de desarrollo
-  : 'https://api-blogcv.onrender.com/api'; // URL de producción
-
+const baseURL = 'https://api-blogcv.onrender.com/api';
 // Crea una instancia de axios con la URL base
 const axiosInstance = axios.create({
   baseURL: baseURL
